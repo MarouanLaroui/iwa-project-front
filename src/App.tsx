d770a4497a01e20ba219c8c1d3f948cf8d5020f2
MarouanLaroui <marouanlaroui2000@gmail.com>
2022-11-14 16:50:54 +0100

    dep(translation): installation of translation tool

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const { t } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
@@ -13,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('main-page-welcome-title')}
         </a>
       </header>
     </div>
