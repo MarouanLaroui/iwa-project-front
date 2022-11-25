505f268e659e57cef4b4a304e0131adb44c04ba1
NathanDjianMartin <ndmroot-git@yahoo.fr>
2022-11-25 19:40:27 +0100

    feat(hooks): add usePut "hook"

---
+++ src/hooks/generic/usePut.ts
@@ -0,0 +1,10 @@
+import richAxios from '../../database/axios/axios-client';
+
+// Not really a hook so it can bypass React's hooks rules
+
+const usePost = <U, V>(
+  path: string,
+  dataToSend: U,
+) => richAxios.put<V>(path, dataToSend);
+
+export default usePost;
