e7a84dcfe9b78f1dbcbf96a303551bcf0e456e54
NathanDjianMartin <ndmroot-git@yahoo.fr>
2022-11-25 12:37:02 +0100

    feat(worker): ad tabs to profile page

--- src/pages/worker-profile-page.tsx
+++ src/pages/worker-profile-page.tsx
@@ -1,16 +1,15 @@
-import { Typography } from '@mui/material';
-import { Stack } from '@mui/system';
 import React from 'react';
+import { Tabs, Tab } from '@mui/material';
+import { Box } from '@mui/system';
 import { useTranslation } from 'react-i18next';
-import BadgeIcon from '@mui/icons-material/Badge';
-import CakeIcon from '@mui/icons-material/Cake';
-import EmailIcon from '@mui/icons-material/Email';
-import TypographyWithIcon from '../components/typography-with-icon';
 import Worker from '../types/worker/Worker';
+import WorkerProfile from '../components/worker-profile';
 
 export default function WorkerProfilePage() {
-  // const params = useParams();
   const { t } = useTranslation();
+  // const params = useParams();
+
+  const [value, setValue] = React.useState(0);
 
   const mockWorker: Worker = {
     id: 'abc-123',
@@ -22,23 +21,20 @@ export default function WorkerProfilePage() {
     hasDrivingLicense: true,
   };
 
-  const dateString = mockWorker.birthDate.toDateString();
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
-    <Stack alignItems="flex-start" spacing="10px">
-      <Typography fontWeight="bold" variant="h5">
-        {t('profile-page-title')}
-      </Typography>
-      <Typography variant="h6">
-        {mockWorker.firstName}
-        {' '}
-        {mockWorker.lastName}
-      </Typography>
-      <TypographyWithIcon text={mockWorker.email} icon={<EmailIcon />} />
-      <TypographyWithIcon text={`${t('born-on')}${dateString.substring(3, dateString.length)}`} icon={<CakeIcon />} />
-      {mockWorker.hasDrivingLicense
-      && <TypographyWithIcon text={t('has-driving-license')} icon={<BadgeIcon />} />}
-
-    </Stack>
+    <>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', marginBottom: '10px' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label={t('personal-information')} />
+          <Tab label={t('search-criterias')} />
+        </Tabs>
+      </Box>
+      {value === 0 && <WorkerProfile worker={mockWorker} />}
+      {value === 1 && <div>Test</div>}
+    </>
   );
 }
