81e6bd28614e663063415c04bad6841b23547241
NathanDjianMartin <ndmroot-git@yahoo.fr>
2022-11-26 17:42:35 +0100

    feat(axios): get token for each request sent

--- src/database/axios/axios-client.ts
+++ src/database/axios/axios-client.ts
@@ -3,7 +3,13 @@ import axios from 'axios';
 const richAxios = axios;
 
 richAxios.defaults.baseURL = `${process.env.REACT_APP_IWA_API_URL}`;
-richAxios.defaults.headers.common = {
-  Authorization: `Bearer ${localStorage.getItem('token')}`,
-};
+richAxios.interceptors.request.use((config) => {
+  if (config && config.headers) {
+    // eslint-disable-next-line no-param-reassign
+    config.headers = {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    };
+  }
+  return config;
+});
 export default richAxios;
